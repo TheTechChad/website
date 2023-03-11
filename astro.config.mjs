4c6b1e1ffb1d488d5256ea236fcdafe33aa06f8d
Ethan Parker <ethanparker10x@gmail.com>
2023-03-11 15:53:40 +0700

    fix basepath

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://thetechchad.github.io',
+  base: '/website',
 });
